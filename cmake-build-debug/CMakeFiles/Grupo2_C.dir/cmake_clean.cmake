d9d3974f937bc1d76bf522983fd4172af37166db
Diego Mancini <DiegoMancini@MacBook-Pro-de-Diego.local>
2017-03-18 19:49:49 -0300

    1.0

---
+++ cmake-build-debug/CMakeFiles/Grupo2_C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Grupo2_C.dir/main.c.o"
+  "Grupo2_C.pdb"
+  "Grupo2_C"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Grupo2_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
